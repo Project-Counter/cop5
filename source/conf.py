3a73dddb2a7479eefebe1554e95c47b0fce9e93e
Bernd Oberknapp <bo@ub.uni-freiburg.de>
2023-04-28 13:36:50 +0200

    Upgrade to Sphinx 5.3.0

--- source/conf.py
+++ source/conf.py
@@ -35,7 +35,7 @@ release = '5.0.3'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '3.5.4'
+needs_sphinx = '5.3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
